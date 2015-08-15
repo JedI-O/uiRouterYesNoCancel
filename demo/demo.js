55482f47c0807d32356ad74423f294e55fbaa704
Paul Podlech <p.podlech@fu-berlin.de>
2015-08-15 15:17:17 +0200

    refactored and changed logic for better efficiency

--- demo/demo.js
+++ demo/demo.js
@@ -1,59 +1,33 @@
+
+
       var app = angular.module('app', ['ui.bootstrap', 'ui.router', 'ui.router.yesNoCancel']);
 
-      app.config(function($stateProvider, $urlRouterProvider){
+      app.config(function($stateProvider, $urlRouterProvider, uiRouterYesNoCancelProvider){
+
+        uiRouterYesNoCancelProvider.setupState({name :'otherState'}, function(){return true}, 'Yes or no before going back?', function(){console.log('yes')}, function(){console.log('no')}, function(){console.log('cancel')});
+
+
         $stateProvider
         .state('someState',
         {
           url: "/state",
-          template: '<div><a ui-sref="otherState">go somehwere else</a><br><form name="myForm" novalidate class="simple-form"><input ng-model="model">Model: <text>{{model}}</text></form><br>{{saveText}}</div>',
-          controller: function($state, $scope, $modal, uiRouterYesNoCancel){
-            uiRouterYesNoCancel.setupState(undefined, function(){return $scope.myForm.$dirty}, 'Test');
-            $scope.saveText = "edit this";
-            $scope.model = $scope.saveText;
-            var disAllowRouting = true;
-            /*$scope.$on('$stateChangeStart', function(event, toState, toParams){
-              if($scope.myForm.$dirty && disAllowRouting){
-              event.preventDefault();
-              var modalHtml = '<div class="modal-body">Do you want to save?</div>' + '<div class="modal-footer"><button class="btn btn-primary" ng-click="yes()">Yes</button><button class="btn btn-primary" ng-click="no()">No</button><button class="btn btn-warning" ng-click="cancel()">Cancel</button></div>';
-
-                        var modalInstance = $modal.open({
-                            template: modalHtml,
-                            controller: function($scope, $modalInstance) {
-                $scope.yes = function() {
-                    $modalInstance.close(0);
-                };
-                $scope.no = function() {
-                    $modalInstance.close(1);
-                };
-                $scope.cancel = function() {
-                    $modalInstance.close(2);
-                };
-            }
-                        });
-
-                        modalInstance.result.then(function(value) {
-                          switch(value){
-                            case 0:  $scope.save(); disAllowRouting = false; $state.go(toState, toParams); break;
-                            case 1:  disAllowRouting = false; $state.go(toState, toParams); break;
-                            case 2:  break;
-                          }
-                        }, function() {
-                            //Modal dismissed
-                        });
-            }});*/
+          template: '<div><a ui-sref="otherState">go somehwere else</a><br><form name="myForm" novalidate class="simple-form"><input ng-model="model"><br></form><br>{{model}}</div>',
+          controller: function($state, $scope, $modal, uiRouterYesNoCancel, $timeout, $window){
+            uiRouterYesNoCancel.setupState(null, function(){return $scope.myForm.$dirty}, null, function(){return $scope.save();});
+            $scope.model = $window.localStorage.getItem('saveText');
             $scope.save = function(){
-              console.log('saved' + $scope.model);
-              $scope.saveText = $scope.model;
+              return $timeout(function(){$window.localStorage.setItem('saveText', $scope.model)}, 1000);
             };
           }
         })
       .state('otherState',
         {
           url: "/other",
-          template: '<div ng-yes-no-cancel="Yes, no or cancel?" ng-yes="test=\'yes\'" ng-no="test=\'no\'" ng-cancel="test=\'cancel\'">Click here!</div><div>{{test}}</div>'
+          template: '<div><a ui-sref="someState">Click here!</a></div>'
         });
 
-      $urlRouterProvider.otherwise('state');
+
+        $urlRouterProvider.otherwise('state');
 
       });
 
--- src/uiRouterYesNoCancel.js
+++ src/uiRouterYesNoCancel.js
@@ -1,52 +1,94 @@
 (function(angular){
+  /* Define the module. */
   var yesNoCancel = angular.module('ui.router.yesNoCancel', ['ui.bootstrap', 'ui.router']);
 
+  /* Run the initialization.
+   * Injecting uiRouterYesNoCancel service here
+   * registers the registerList collected by the provider.
+   */
+  yesNoCancel.run(['uiRouterYesNoCancel', '$rootScope', '$modal', '$state', '$q', function(a, $rootScope, $modal, $state, $q){
+      var proceed = function(fromState, toState, toParams){
+        return function(){
+          fromState.uiRouterYesNoCancel.allowRouting = true; $state.go(toState, toParams);
+        }
+      };
+      /* on $stateChangeStart */
+      $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+        /* check if routing is allowed. */
+        if(!fromState.uiRouterYesNoCancel || fromState.uiRouterYesNoCancel.allowRouting || !fromState.uiRouterYesNoCancel.condition()){
+          return;
+        }
+        /* if not allowed:  */
+        event.preventDefault();
+
+        /* set up the $modal  settings and open the modal*/
+        var modalHtml = '<div class="modal-body">' + fromState.uiRouterYesNoCancel.message + '</div>' + '<div class="modal-footer"><button class="btn btn-primary" ng-click="yes()">Yes</button><button class="btn btn-primary" ng-click="no()">No</button><button class="btn btn-warning" ng-click="cancel()">Cancel</button></div>';
+
+        var modalInstance = $modal.open({
+          template: modalHtml,
+          controller: function($scope, $modalInstance) {
+            $scope.yes = function() {
+              $modalInstance.close(0);
+            };
+            $scope.no = function() {
+              $modalInstance.close(1);
+            };
+            $scope.cancel = function() {
+              $modalInstance.dismiss();
+            };
+          }
+        });
+
+        /* the result promise is resolved when the modalInstance closes or is dismissed */
+        modalInstance.result.then(function(result){
+          /* I close, it's closed by yes or no button.
+           * So call the appropriate functions and then route.
+           */
+          switch(result){
+            case 0: $q.when(fromState.uiRouterYesNoCancel.yes()).then(proceed(fromState, toState, toParams)); break;
+            case 1: $q.when(fromState.uiRouterYesNoCancel.no()).then(proceed(fromState, toState, toParams)); break;
+          }
+        },
+        function(){
+          /* If dismissed, call the cancel functoin and then route back to previous state (only necessary for URL history). */
+          $q.when(fromState.uiRouterYesNoCancel.cancel()).then(function(){$state.go(fromState, fromParams)});
+        });
+      });
+  }]);
+
   yesNoCancel.provider('uiRouterYesNoCancel', function(){
-    this.$get = ['$state', '$rootScope', '$modal', function($state, $rootScope, $modal){
+    var registerList = [];
+
+    this.setupState = function(fromState, condition, message, yes, no, cancel){
+      registerList.push(arguments);
+    }
+
+    this.$get = ['$state', '$rootScope', '$modal', '$urlRouter', function($state, $rootScope, $modal, $urlRouter){
+      var setup = false;
+      var self = this;
+
       this.setupState = function(fromState, condition, message, yes, no, cancel){
         fromState = fromState || $state.$current;
         fromState = $state.get(fromState.name);
-        console.log(fromState);
-        message = message || 'Do you want to save?';
         var noop = function(){};
-        fromState.yes = yes || noop;
-        fromState.no = no || noop;
-        fromState.cancel = cancel || noop;
-        fromState.condition = condition;
-
-        fromState.allowRouting = false;
-
-        $rootScope.$on('$stateChangeStart', function(event, toState, toParams){
-          if(fromState.allowRouting || fromState.condition()){
-            return false;
-          }
-          event.preventDefault();
-          var modalHtml = '<div class="modal-body">' + message + '</div>' + '<div class="modal-footer"><button class="btn btn-primary" ng-click="yes()">Yes</button><button class="btn btn-primary" ng-click="no()">No</button><button class="btn btn-warning" ng-click="cancel()">Cancel</button></div>';
-
-          var modalInstance = $modal.open({
-            template: modalHtml,
-            controller: function($scope, $modalInstance) {
-              $scope.yes = function() {
-                $modalInstance.close(0);
-              };
-              $scope.no = function() {
-                $modalInstance.close(1);
-              };
-              $scope.cancel = function() {
-                $modalInstance.close(2);
-              };
-            }
-          });
-
-          modalInstance.result.then(function(result){
-            switch(result){
-              case 0: fromState.yes(); fromState.
-            }
-          });
-
-
-        });
+        fromState.uiRouterYesNoCancel = {};
+        fromState.uiRouterYesNoCancel.message = message || 'Do you want to save?';
+        fromState.uiRouterYesNoCancel.yes = yes || noop;
+        fromState.uiRouterYesNoCancel.no = no || noop;
+        fromState.uiRouterYesNoCancel.cancel = cancel || noop;
+        fromState.uiRouterYesNoCancel.condition = condition;
+        fromState.uiRouterYesNoCancel.allowRouting = false;
+        fromState.onEnter = function(){
+          fromState.uiRouterYesNoCancel.allowRouting = false;
+        }
       };
+
+      angular.forEach(registerList, function(args){
+        self.setupState.apply(self, args);
+      });
+
+      registerList = [];
+
       return this;
     }];
   });
